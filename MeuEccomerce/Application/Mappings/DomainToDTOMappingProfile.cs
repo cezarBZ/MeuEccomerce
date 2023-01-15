db776a92fc73c3d3b22e68786ba847e90c12c2a0
Cezar dos Santos Bezerra <cezar.bezerra@programmers.com.br>
2023-01-15 12:09:09 -0300

    initial project creation and configuration

---
+++ MeuEccomerce/Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MeuEccomerce.Domain.AggregatesModel.CategoryAggregate;
+using MeuEccomerce.Domain.AggregatesModel.ProductAggregate;
+
+namespace MeuEccomerce.API.Application.Mappings;
+
+public class DomainToDTOMappingProfile : Profile
+{
+    public DomainToDTOMappingProfile()
+    {
+        //CreateMap<Category, CategoriaDTO>().ReverseMap();
+        //CreateMap<Product, ProdutoDTO>().ReverseMap();
+    }
+}
---
+++ MeuEccomerce/IoC/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using MeuEccomerce.API.Application.Mappings;
+using MeuEccomerce.Domain.AggregatesModel.CategoryAggregate;
+using MeuEccomerce.Domain.AggregatesModel.ProductAggregate;
+using MeuEccomerce.Domain.Core.Data;
+using MeuEccomerce.Infrastructure.Data;
+using MeuEccomerce.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeuEccomerce.API.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+           IConfiguration configuration)
+    {
+
+        services.AddDbContext<ApplicationDataContext>(options =>
+           options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+            ), b => b.MigrationsAssembly(typeof(ApplicationDataContext).Assembly.FullName)));
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+        return services;
+    }
+}
