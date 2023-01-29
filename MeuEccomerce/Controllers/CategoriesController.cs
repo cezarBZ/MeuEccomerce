c4adc159025e4f6ab74879ef0857f58c3dd27232
Cezar dos Santos Bezerra <cezar.bezerra@programmers.com.br>
2023-01-29 10:13:55 -0300

    controllers created

---
+++ MeuEccomerce/Controllers/CategoriesController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using MediatR;
+using MeuEccomerce.API.Application.Commands.Category;
+using MeuEccomerce.API.Application.Models.DTO_s;
+using MeuEccomerce.API.Application.Query;
+using MeuEccomerce.API.Application.Query.Categories;
+using MeuEccomerce.Domain.AggregatesModel.CategoryAggregate;
+using MeuEccomerce.Domain.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MeuEccomerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    [Route("{Id:int}")]
+    public async Task<IActionResult> GetById(int Id)
+    {
+        var category = await _mediator.Send(new GetCategoryByIdQuery(Id));
+        return Ok(category);
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllCategories()
+    {
+        var category = await _mediator.Send(new GetAllCategoriesQuery());
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateCategoryCommand command)
+    {
+        var cmd = await _mediator.Send(command);
+
+        if (!cmd)
+            return BadRequest();
+        return Ok("Deu bom");
+    }
+
+    [HttpPatch]
+    [Route("{Id:int}/disable")]
+    public async Task<IActionResult> Disable(int Id)
+    {
+        DisableCategoryCommand category = new(Id);
+
+        var cmd = await _mediator.Send(category);
+
+        if (!cmd)
+            return BadRequest();
+        return Ok("Deu bom");
+    }
+
+    [HttpPatch]
+    [Route("{Id:int}/enable")]
+    public async Task<IActionResult> Enable(int Id)
+    {
+        EnableCategoryCommand category = new(Id);
+
+        var cmd = await _mediator.Send(category);
+
+        if (!cmd)
+            return BadRequest();
+        return Ok("Deu bom");
+    }
+
+}
---
+++ MeuEccomerce/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using MeuEccomerce.API.Application.Commands.Product;
+using MeuEccomerce.API.Application.Query.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuEccomerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("{Id:Guid}")]
+    public async Task<IActionResult> GetById(Guid Id)
+    {
+        var product = await _mediator.Send(new GetProductByIdQuery(Id));
+        return Ok(product);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var product = await _mediator.Send(new GetAllProductsQuery());
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] AddProductCommand command)
+    {
+        var cmd = await _mediator.Send(command);
+
+        if (!cmd)
+            return BadRequest();
+        return Ok("Deu bom");
+    }
+
+    [HttpDelete]
+    [Route("{Id:Guid}")]
+    public async Task<IActionResult> Delete(Guid Id)
+    {
+        DeleteProductCommand product = new(Id);
+        var cmd = await _mediator.Send(product);
+        if (!cmd)
+            return BadRequest();
+        return Ok("Deu bom");
+        
+    }
+
+}
