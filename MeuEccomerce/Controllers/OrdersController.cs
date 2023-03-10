61b103eff350deb699fb27459b5c4378f70ea6a7
Cezar dos Santos Bezerra <cezar.bezerra@programmers.com.br>
2023-03-09 21:15:46 -0300

    Add OrderController

---
+++ MeuEccomerce/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MeuEccomerce.API.Application.Commands.Orders;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuEccomerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateOrderCommand command)
+        {
+            var cmd = _mediator.Send(command);
+            return Ok(cmd);
+        }
+    }
+}
